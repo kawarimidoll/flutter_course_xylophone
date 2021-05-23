45d94255314de87731e3c0de2919c9c6363465de
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-23 20:05:34 +0900

    add noteButtons

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-// import 'package:audioplayers/audio_cach.dart'; // URI doesn't exist
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    List<Color> colors = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.lightGreen,
+      Colors.green,
+      Colors.blue,
+      Colors.purple,
+    ];
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click me!'),
-            ),
+          child: Column(
+            children: colors
+                .asMap()
+                .entries
+                .map((e) => noteButton(num: e.key + 1, color: e.value))
+                .toList(),
           ),
         ),
+      ),
+    );
+  }
+
+  void playSound(int num) {
+    final player = AudioCache();
+    player.play('note$num.wav');
+  }
+
+  Widget noteButton({required int num, required Color color}) {
+    return TextButton(
+      onPressed: () => playSound(num),
+      child: Container(
+        width: 300.0,
+        height: 40.0,
+        margin: EdgeInsets.all(3.0),
+        color: color,
       ),
     );
   }
