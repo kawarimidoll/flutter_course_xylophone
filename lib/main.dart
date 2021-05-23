f9e5c521ef92ba749d43d14466d7f82215334aa3
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-23 17:57:53 +0900

    add audiocache

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+// import 'package:audioplayers/audio_cach.dart'; // URI doesn't exist
 
-void main() {
-  runApp(XylophoneApp());
-}
+void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click me!'),
+            ),
+          ),
         ),
       ),
     );
